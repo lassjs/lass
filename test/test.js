b06c028afb7373ed2a46a8ab165937b37f352b46
Nick Baugh <niftylettuce@gmail.com>
2017-09-03 11:50:07 -0400

    Fixed broken tests

--- test/test.js
+++ test/test.js
@@ -4,19 +4,24 @@ import sao from 'sao';
 
 const template = path.join(__dirname, '..');
 
-const name = 'lass';
+const defaults = {
+  name: 'lass',
+  description: 'my project description',
+  license: 'MIT',
+  version: '0.0.1',
+  author: 'Nick Baugh',
+  email: 'niftylettuce@gmail.com',
+  website: 'http://niftylettuce.com',
+  username: 'niftylettuce'
+};
 
 test('defaults', async t => {
-  const stream = await sao.mockPrompt(template, {
-    name: 'my-package-name',
-    description: 'my project description',
-    license: 'MIT',
-    version: '0.0.1',
-    author: 'Nick Baugh',
-    email: 'niftylettuce@gmail.com',
-    website: 'http://niftylettuce.com',
-    username: 'niftylettuce'
-  });
+  const stream = await sao.mockPrompt(
+    template,
+    Object.assign({}, defaults, {
+      name: 'my-package-name'
+    })
+  );
   t.snapshot(stream.fileList, 'generated files');
   const content = stream.fileContents('README.md');
   t.snapshot(content, 'content of README.md');
@@ -24,9 +29,12 @@ test('defaults', async t => {
 
 test('invalid name', async t => {
   const error = await t.throws(
-    sao.mockPrompt(template, {
-      name: 'Foo Bar Baz Beep'
-    })
+    sao.mockPrompt(
+      template,
+      Object.assign({}, defaults, {
+        name: 'Foo Bar Baz Beep'
+      })
+    )
   );
   t.regex(
     error.message,
@@ -36,51 +44,61 @@ test('invalid name', async t => {
 
 test('invalid version', async t => {
   const error = await t.throws(
-    sao.mockPrompt(template, {
-      name,
-      version: 'abcdef'
-    })
+    sao.mockPrompt(
+      template,
+      Object.assign({}, defaults, {
+        version: 'abcdef'
+      })
+    )
   );
   t.regex(error.message, /Invalid semver version/);
 });
 
 test('invalid email', async t => {
   const error = await t.throws(
-    sao.mockPrompt(template, {
-      name,
-      email: 'niftylettuce'
-    })
+    sao.mockPrompt(
+      template,
+      Object.assign({}, defaults, {
+        email: 'niftylettuce'
+      })
+    )
   );
   t.regex(error.message, /Invalid email/);
 });
 
 test('invalid website', async t => {
   const error = await t.throws(
-    sao.mockPrompt(template, {
-      name,
-      website: 'niftylettuce'
-    })
+    sao.mockPrompt(
+      template,
+      Object.assign({}, defaults, {
+        website: 'niftylettuce'
+      })
+    )
   );
   t.regex(error.message, /Invalid URL/);
 });
 
 test('invalid username', async t => {
   const error = await t.throws(
-    sao.mockPrompt(template, {
-      name,
-      username: '$$$'
-    })
+    sao.mockPrompt(
+      template,
+      Object.assign({}, defaults, {
+        username: '$$$'
+      })
+    )
   );
   t.regex(error.message, /Invalid GitHub username/);
 });
 
 test('invalid repo', async t => {
   const error = await t.throws(
-    sao.mockPrompt(template, {
-      name,
-      username: 'lassjs',
-      repo: 'https://bitbucket.org/foo/bar'
-    })
+    sao.mockPrompt(
+      template,
+      Object.assign({}, defaults, {
+        username: 'lassjs',
+        repo: 'https://bitbucket.org/foo/bar'
+      })
+    )
   );
   t.regex(
     error.message,
