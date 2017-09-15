8a0564faf6513c1a3cc0a4b7224d42a27357cbc4
Alexis Tyler <xo@wvvw.me>
2017-09-15 19:11:45 +0930

    add auto-bind

--- template/index.js
+++ template/index.js
@@ -1,7 +1,11 @@
+const autoBind = require('auto-bind');
+
 class Script {
   constructor(config) {
     config = Object.assign({}, config);
     this._name = config.name || 'script';
+
+    autoBind(this);
   }
   renderName() {
     return this._name;
