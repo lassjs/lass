875d438a3f48efa84f74a423aa57ed064335a4f9
imed jaberi <imed_jebari@hotmail.fr>
2020-06-15 01:36:20 +0100

    Fix #/ on router (#58)
    * use ' over " 🌂
    
    * use routeMode opts --vue-router 🥽

--- config.js
+++ config.js
@@ -1,18 +1,19 @@
 docute.init({
-  debug: true,
-  title: 'Lass',
-  repo: 'lassjs/lass',
-  'edit-link': 'https://github.com/lassjs/lass/tree/master/',
-  twitter: 'niftylettuce',
-  nav: {
-    default: [
-      {
-        title: 'Scaffold a modern package boilerplate for Node.js',
-        path: '/'
-      }
-    ]
-  },
-  plugins: [
-    docuteEmojify()
-  ]
+	debug: true,
+	routerMode: 'history',
+	title: 'Lass',
+	repo: 'lassjs/lass',
+	'edit-link': 'https://github.com/lassjs/lass/tree/master/',
+	twitter: 'niftylettuce',
+	nav: {
+		default: [
+			{
+				title: 'Scaffold a modern package boilerplate for Node.js',
+				path: '/'
+			}
+		]
+	},
+	plugins: [
+		docuteEmojify()
+	]
 });
